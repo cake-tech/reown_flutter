13fa51d78e027f037f000e2929c9a70d8c4f25da
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-10-07 22:09:36 -0400

    update to forked yttrium, bundle .aar without .so

--- scripts/build_native_deps.sh
+++ scripts/build_native_deps.sh
@@ -5,18 +5,12 @@ set -x -e
 
 if [ ! -e yttrium/.git ]; then
     rm -rf yttrium
-    git clone https://github.com/reown-com/yttrium
+    git clone https://github.com/cake-tech/yttrium
 fi
 cd yttrium
-# git checkout ed8e8f5af2029406263be5993e484c3a69c1db7a
 git reset --hard
-git checkout 9f81ab8e0fb879a994392d603b7908b2104d1735
+git checkout 7a8b5bd01876662158db334381db8592c66952e6
 git reset --hard
-
-sed -i.bak "s/-i ''/-i.bak/g" build-kotlin.sh
-sed -i.bak "s/--bin uniffi-bindgen generate/-p kotlin-ffi --bin uniffi-bindgen generate/g" build-kotlin.sh
-sed -i.bak "s/stat -f%z/echo stat -f%z/g" build-kotlin.sh
-sed -i.bak "s/ -t arm64-v8a/ -t arm64-v8a -t x86_64/g" build-kotlin.sh
 
 cargo install cargo-ndk
 ENABLE_STRIP=false PROFILE=release bash -x ./build-kotlin.sh
